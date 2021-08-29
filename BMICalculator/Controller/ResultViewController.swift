6caafcee4948ba96719c9ce33d8df81ced7dfa88
Mohammed Yaqob <mohammed.yaqob.98@gmail.com>
2021-08-29 06:17:36 +0200

    Initial Commit

---
+++ BMICalculator/Controller/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  BMICalculator
+//
+//  Created by Mohammed Yaqob's MacBook on 05/09/2020.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ BMICalculator/Model/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Mohammed Yaqob's MacBook on 05/09/2020.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
